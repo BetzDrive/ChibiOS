c4efb79345dd2b8439171d1dea5c8a2c349f79f7
Greg Balke <gbalke@berkeley.edu>
2019-04-23 23:34:34 -0700

    Adjusting folder structure and configs for stm libs

---
+++ ext/stm32lib/stm32lib.mk
@@ -0,0 +1,34 @@
+# STM32F4 STDPERIPH files.
+STM32SRC = 	${CHIBIOS}/ext/stm32lib/src/misc.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_dma.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_rcc.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_adc.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_exti.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_rng.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_flash.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_rtc.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_crc.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_fsmc.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_sdio.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_cryp_aes.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_spi.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_cryp.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_hash.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_syscfg.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_cryp_des.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_hash_md5.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_tim.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_cryp_tdes.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_hash_sha1.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_usart.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_dac.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_i2c.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_wwdg.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_dbgmcu.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_iwdg.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_dcmi.c \
+			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_pwr.c
+#			${CHIBIOS}/ext/stm32lib/src/stm32f4xx_gpio.c
+
+STM32INC = ${CHIBIOS}/ext/stm32lib/inc
+
